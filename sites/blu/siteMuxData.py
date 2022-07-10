1747af6d503cd9440d8fa483954dd21e1c0392bc
Your Name <you@example.com>
2022-07-09 22:29:05 -0500

    Add Default Track Key Change Default Track Parsing during Remuxing

--- sites/blu/siteMuxData.py
+++ sites/blu/siteMuxData.py
@@ -11,13 +11,14 @@ class Blu(MuxHelper):
         super().__init__()
 
     def createMKV(self, movieTitle, chapters, fileName, remuxConfig):
-        super().createMKV(movieTitle, chapters, fileName)
+        super().createMKV(movieTitle, chapters, fileName,remuxConfig)
         os.chdir(os.path.dirname(os.path.realpath(__file__)))
         tempdir = tempfile.TemporaryDirectory()
         mediainfo = os.path.join(tempdir.name,"media.txt")
         with open(mediainfo,"w") as p:
             p.write(remuxHelper.getMediaInfo(fileName))
-        command = ["/usr/local/bin/mine/remux/bin/python3", "../vdator/vdator/main.py",
+        
+        command = ["/usr/local/bin/mine/remux/bin/python3", "../../vdator/vdator/main.py",
                 mediainfo, remuxHelper.getBdinfo(remuxConfig), remuxHelper.getEac3to(remuxConfig)]
         with subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True, bufsize=1) as p:
             for line in p.stdout:
