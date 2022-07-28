8e780db4fb577827e593c76c012848529a7a1e48
Your Name <you@example.com>
2022-07-28 14:13:08 -0500

    added wine

--- sites/animebytes/siteMuxData.py
+++
@@ -1,7 +0,0 @@
-from sites.base.siteMuxData import MuxOBj
-
-
-
-class AnimeBytes(MuxOBj):
-    def __init__(self):
-        super().__init__()
--- sites/beyondHD/siteTrackSorter.py
+++
@@ -1,8 +0,0 @@
-from sites.base.siteTrackSorter import siteTrackSorter
-
-
-class BeyondHD(siteTrackSorter):
-    def __init__(self):
-        super().__init__()
-   
-    
