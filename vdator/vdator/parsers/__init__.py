e11c1a2051d8ebff789b9302f01e9adb6d3ddb74
Your Name <you@example.com>
2022-07-28 14:13:08 -0500

    added wine

--- vdator/vdator/parsers/__init__.py
+++
@@ -1,5 +0,0 @@
-from .bdinfo_parser import BDInfoParser
-from .codecs_parser import CodecsParser
-from .media_info_parser import MediaInfoParser
-from .paste_parser import PasteParser
-from .url_parser import URLParser
