6e3148c96091705855cee1ef814c30e9c3c695b7
Your Name <you@example.com>
2022-08-04 14:24:21 -0500

    fix positional arguments

--- app.py
+++ app.py
@@ -6,25 +6,30 @@ import demux.demux as demuxAction
 import remux.remux as remuxAction
 
 
+def demux(args):
+    demuxAction.demux(args)
+
+
+def remux(args):
+    remuxAction.Remux(args)
+
+
 def main():
-
-   #!/usr/bin/env python3.10
-
-
     parser = argparse.ArgumentParser(prog='Remuxer', add_help=False)
 
     subparsers = parser.add_subparsers(help='main positional commands')
 
-    parser_d = subparsers.add_parser('demux', help='Demux BDMV/ISO to Raw Files')
+    parser_d = subparsers.add_parser(
+        'demux', help='Demux BDMV/ISO to Raw Files')
     parser_d.add_argument('inpath', help="Location of BDMV/ISO Files")
     parser_d.add_argument('outpath', default=os.getcwd(),
-                        help="Location Raw Files Folder Should be Created")
+                          help="Location Raw Files Folder Should be Created")
     parser_d.add_argument('-p', '--sortpref', default="size",
-                        choices=["size", "order"], help="What Order to use to Rank Tracks")
+                          choices=["size", "order"], help="What Order to use to Rank Tracks")
     parser_d.add_argument('-sl', '--sublang', nargs='*',
-                        default=[], help="Matches subtitles with these languages")
+                          default=[], help="Matches subtitles with these languages")
     parser_d.add_argument('-al', '--audiolang', nargs='*',
-                        default=[], help="Matches audio with these languages")
+                          default=[], help="Matches audio with these languages")
     parser_d.add_argument(
         '-o', '--ocr', choices=['enabled', 'sublang', 'default', "all", "english"], help="OCR gracks outputs to JSON")
     parser_d.add_argument(
@@ -32,43 +37,39 @@ def main():
     parser_d.add_argument('-k', '--keepocr', action='store_true', help="Take Screen Shot of specific Tracks\ndefaults to enabled tracks, \
     can be changed with --ocr")
     parser_d.add_argument('-dc', '--dontconvert',  action='store_true',
-                        help="Don't convert any lossless track to FLAC")
+                          help="Don't convert any lossless track to FLAC")
     parser_d.add_argument('-sp', '--splitplaylist', action='store_true',
-                        help="During TV Mode extract each mt2s as seperate episode")
+                          help="During TV Mode extract each mt2s as seperate episode")
 
+    parser_d.set_defaults(func=demux)
 
-    parser_r = subparsers.add_parser('remux', help='Remux Raw files into mkv(s)')
+    parser_r = subparsers.add_parser(
+        'remux', help='Remux Raw files into mkv(s)')
     parser_r.add_argument('inpath', help="Location of RawFiles and JSON File")
     parser_r.add_argument('outpath', default=os.getcwd(),
-                        help="Location mkv(s) should be created")
+                          help="Location mkv(s) should be created")
     parser_r.add_argument('-sn', '--skipnamecheck',
-                        action='store_true', help="Skip Verification of Filenames")
+                          action='store_true', help="Skip Verification of Filenames")
     parser_r.add_argument('-g', '--group', default="Unknown",
-                        help="Change the Group Tag For Final mkv(s")
+                          help="Change the Group Tag For Final mkv(s")
     parser_r.add_argument('-fm', '--forcemovie',  action='store_true',
-                        help="Force the output mkv to use movie syntax")
+                          help="Force the output mkv to use movie syntax")
     parser_r.add_argument('-oa', '--outargs', default="",
-                        help="Pass mkvmerge global options")
+                          help="Pass mkvmerge global options")
+    parser_r.set_defaults(func=remux)
 
     #This seems to be the only way to
     # A have positional arguments
     #Easily Apply some argumetns to all subgroups
     for pars in [parser_d, parser_r]:
         pars.add_argument('-s', '--site', required=False, default=None,
-                        help="Customize Output based on this sites rules, and general content")
+                          help="Customize Output based on this sites rules, and general content")
         pars.add_argument(
             '--log-level', default='INFO',
             help='logging level')
 
-
     args = parser.parse_args()
-
-
-    if args.demux:
-        demuxAction.demux(args)
-
-    elif args.remux:
-        remuxAction.Remux(args)
+    args.func(args)
 
 
 if __name__ == "__main__":
