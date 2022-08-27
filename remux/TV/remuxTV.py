422868ad3cdcca736a83c9af177561941edd57cc
John Doe <johndoe@example.com>
2022-08-27 09:39:07 -0500

    syntax

--- remux/TV/remuxTV.py
+++ remux/TV/remuxTV.py
@@ -108,7 +108,7 @@ def Remux(args):
         print(f"New File at {ele}\n")
         mediainfo = MediaInfo.parse(ele, output="", full=False)
         print(f"\n\n{mediainfo}\n\n")
-    print(f"As a Reminder the output location is: {os.path.dirnanme(fileNameList[0])}")
+    print(f"As a Reminder the output location is: {os.path.dirname(fileNameList[0])}")
 
 
 def ProcessBatch(fileName, remuxConfig, muxGenerator, movieObj, outargs):
