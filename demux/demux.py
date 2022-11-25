6b3daeb94c37624b4000b00142b4eaa2a2a7e9f0
Your Name <you@example.com>
2022-11-25 02:02:01 -0600

    add some missing requirements

--- demux/demux.py
+++ demux/demux.py
@@ -5,7 +5,11 @@ import demux.TV.demuxTV as demuxTV
 
 def demux(args):
     options = ["Movie", "TV"]
-    if utils.singleSelectMenu(options, "What Type of Media are you Demuxing?") == "Movie":
-        demuxMovie.demux(args)
-    else:
-        demuxTV.demux(args)
+    try:
+        print(options[3])
+        if utils.singleSelectMenu(options, "What Type of Media are you Demuxing?") == "Movie":
+            demuxMovie.demux(args)
+        else:
+            demuxTV.demux(args)
+    except Exception as E:
+        print(E)
