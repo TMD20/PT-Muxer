66f70cac5f7cb6d68822094350e5887febbe65b6
TMD20 <109320934+TMD20@users.noreply.github.com>
2022-12-22 12:32:56 -0600

    formatting

--- remux/action.py
+++ remux/action.py
@@ -10,38 +10,34 @@ import tools.exit as exit
 import tools.logger as logger
 
 
-
 def endProcess():
-    if remuxObj!=None and remuxObj.success==True:
+    if remuxObj != None and remuxObj.success == True:
         logger.print("Remuxing Complete")
         paths.deleteTempDirs()
     else:
         quit()
-    
 
-    
+
 def remux(args):
-    #Help user out delete all temp directories
+    # Help user out delete all temp directories
     paths.deleteTempDirs()
     global remuxObj
-    remuxObj=None
+    remuxObj = None
     with exit.GracefulExit(endProcess):
         try:
             # Variables
             if args.forcemovie:
-                remuxObj=remuxMovies.Remux(args)
+                remuxObj = remuxMovies.Remux(args)
 
             elif utils.singleSelectMenu(["Movie", "TV"], "What Type of Media do you want to Remux?") == "Movie":
-                remuxObj=remuxMovies.Remux(args)
+                remuxObj = remuxMovies.Remux(args)
             else:
-                remuxObj=remuxTV.Remux(args)
+                remuxObj = remuxTV.Remux(args)
             remuxObj()
         except Exception as E:
-            logger.print(traceback.format_exc(),style="white")
-            logger.print(E,style="white")
+            logger.print(traceback.format_exc(), style="white")
+            logger.print(E, style="white")
         quit()
-        # argStr='\n'.join(list(map(lambda x:f"Argument {x[0]}: {x[1]}",vars(args).items())))       
+        # argStr='\n'.join(list(map(lambda x:f"Argument {x[0]}: {x[1]}",vars(args).items())))
         # if utils.singleSelectMenu(["Yes", "No"], f"{argStr}\n\nRemux more Media with same commandline args") == "No":
         #          break
-
-
