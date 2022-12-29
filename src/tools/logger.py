84d146ca4ee2c6884dfe388e306adcbe69983e51
TMD20 <109320934+TMD20@users.noreply.github.com>
2022-12-29 13:23:03 -0600

    add docstrings to logger

--- src/tools/logger.py
+++ src/tools/logger.py
@@ -7,28 +7,61 @@ from rich.markdown import Markdown
 
 logger=logging.getLogger("PTMuxer")
 
-def print(message,style=None):
-    if style==None:
-        style="bold green"
+def print(message,style="bold green"):
+    """
+    Uses rich module to print a normal output to console
+
+    Args:
+        message (str): message to print
+        style (str, optional): style information for text to pass to rich. Defaults to bold green.
+    """
     console = Console()
     console.print(message,style=style)
 
 
 
-def handleLogMessage(obj, markup=True,style=None,**kwargs):
+def handleLogMessage(message, markup=True,style="bold green",**kwargs):
+    """
+    Helper function to levage rich to enhance printing of logs
+
+    Args:
+        message (str): message to pass to rich
+        markup (bool, optional): markup information to pass to rich. Defaults to True.
+        style (str, optional): style information to pass to rich. Defaults to bold green.
+
+    Returns:
+        obj: return a console capture
+    """
     console=Console(markup=markup,style=style)
-    if style==None:
-        style="bold green"
-    markdown=Markdown(obj,style=style)
+    
+        
+    markdown=Markdown(message,style=style)
     with console.capture() as capture:
         console.print(markdown)
     return capture.get()
         
 def loggingRevamp(function,style=None):
+    """
+    returns a console capture
+
+    Args:
+        function (function): function to pass arguments to
+        style (str, optional): style to pass to function. Defaults to None.
+
+    Returns:
+        function: returns function for logger
+    """
     return lambda message,markup=True,style=style:  function(handleLogMessage(message,markup,style=style))
     
 
 def setUpLogging(levelArg):
+    """
+    Changes default log function to modified log functions with rich enhanchment
+    sets logger default
+
+    Args:
+        levelArg (str): level to set the logger to
+    """
     logger.setLevel(levelArg)
     logger.propagate=False
     stream = logging.StreamHandler()
