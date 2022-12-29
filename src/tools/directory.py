9332f0f637e18cc98f3d42f25d2f3b7dfe5b1a84
TMD20 <109320934+TMD20@users.noreply.github.com>
2022-12-29 13:11:08 -0600

    add docstrings to directory class

--- src/tools/directory.py
+++ src/tools/directory.py
@@ -4,6 +4,10 @@ import os
 import src.tools.paths as paths
 
 class cwd:
+    """
+    This class creates a context provider for changing the directory on entrance
+    returns to original directory at exit
+    """
     def __init__( self,dir ):
         self._olddir=os.getcwd()
         self._dir=dir
